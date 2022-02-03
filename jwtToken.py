00b9d6ca96f80c3c3f2ee082a4d2b0fa67075576
SanthD <prasanthsatsangi@gmail.com>
2022-02-03 17:30:32 +0530

    feat: jwt module added

--- jwtToken.py
+++ jwtToken.py
@@ -1,6 +1,12 @@
 import jwt
-encoded = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
-print(encoded)
+#encoded = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
+#print(encoded)
 #eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzb21lIjoicGF5bG9hZCJ9.Joh1R2dYzkRvDkqv3sygm5YyK8Gi4ShZqbhK2gxcs2U
-jwt.decode(encoded, "secret", algorithms=["HS256"])
+#jwt.decode(encoded, "secret", algorithms=["HS256"])
 #{'some': 'payload'}
+
+def create_jwt_toke(payload,key,algo):
+    return jwt.encode(payload, key,algorithm=algo)
+
+def decode_jwt_token(token,key,algo):
+    return jwt.decode(token,key,algorithm=algo)
